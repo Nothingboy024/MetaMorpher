40e06c7f46e29af07eb5c3ee3e1865c72c380d6e
Nothingboy024 <156678321+Nothingboy024@users.noreply.github.com>
2025-07-11 18:29:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,4 +20,4 @@ SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with
 AUTH_USERS = int(os.environ.get("AUTH_USERS", '7364106679'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", --1002457918476")
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", "-1002457918476")
